e25cef2e31e041b9b8daf2b4e5f26b761cfdf8f7
Cameron Rosenberger <crosenberger17@gmail.com>
2021-11-03 10:19:38 -0700

    updated readme and script.js with prior work

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-Data Structures and Algorithms
-Since I have been on AlogExpert and ZeroToMastery, my commits have been lacking on Github.
-This repo will track all the time I am spending learning these skills backwards and forwards.
+# Data Structures and Algorithms</br>
+
+### Since I have been on AlogExpert and ZeroToMastery, my commits have been lacking on Github.</br>
+
+### This repo will track all the time I am spending learning these skills backwards and forwards.</br>
+
+# Sections Completed:</br>
+
+- [x] Big-O Notation
+- [x] Arrays
+- [x] Hash Tables
+- [] Linked Lists
+- [] Stacks and Queues
+- [] Trees
+- [] Graphs
+- [] Recursion
+- [] Sorting
+- [] Searching + BFS + DFS
+- [] Dynamic Programming
--- script.js
+++ script.js
@@ -25,3 +25,321 @@ function reverse2(str) {
 const reverse3 = (str) => str.split("").reverse().join("");
 
 reverse3("Hi My name is Cameron");
+
+//Merging sorted arrays
+
+function mergeSortedArrays(array1, array2) {
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+
+  //Check input
+  if (array1.length === 0) {
+    return array2;
+  }
+  if (array2.length === 0) {
+    return array1;
+  }
+
+  while (array1Item || array2Item) {
+    console.log(array1Item, array2Item);
+    if (!array2Item || array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+  }
+
+  return mergedArray;
+}
+
+mergeSortedArrays([0, 3, 4, 31], [3, 4, 6, 30]);
+
+// HashTable Exercise
+
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+    return keysArray;
+  }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set("grapes", 10000);
+myHashTable.set("apples", 54);
+myHashTable.set("oranges", 2);
+myHashTable.keys();
+
+// First recurring character exercise
+
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+function firstRecurringCharacter(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+  return undefined;
+} // 0(n^2)
+
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    } else {
+      map[input[i]] = i;
+    }
+  }
+  console.log(map);
+  return undefined;
+} // 0(n)
+
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// This won't work in first version, because it takes i at [0] and iterates over each value so it will find 2 instead of 5.
+// return 5 because the pairs are before 2,2
+
+firstRecurringCharacter2([2, 5, 5, 2, 3, 5, 1, 2, 4]);
+
+// Linked Lists
+
+// Create the below linked list:
+// myLinkedList = {
+//   head: {
+//     value: 10
+//     next: {
+//       value: 5
+//       next: {
+//         value: 16
+//         next: null
+//       }
+//     }
+//   }
+// };
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const firstNode = {
+      value: value,
+      next: null
+    };
+    firstNode.next = this.head;
+    this.head = firstNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = {
+      value: value,
+      next: null
+    };
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.remove(2);
+
+// console.log(myLinkedList)
+
+// add a method remove() to the linked list that deletes a node to the specified index.
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null
+    };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    //Check for proper parameters;
+    if (index >= this.length) {
+      console.log("yes");
+      return this.append(value);
+    }
+
+    const newNode = {
+      value: value,
+      next: null
+    };
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    // Check Parameters
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+myLinkedList.remove(2);
